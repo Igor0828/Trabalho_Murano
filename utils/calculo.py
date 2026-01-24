2a0976a305ad1534790f445281fcf57cb438661d
Igor0828 <130799283+Igor0828@users.noreply.github.com>
2026-01-24 15:48:51 -0300

    Update calculo.py

--- utils/calculo.py
+++ utils/calculo.py
@@ -1,5 +1,2 @@
 def calcular_custo_total(custos: dict) -> float:
-    return sum(custos.values())
-
-def calcular_parcela(total: float, parcelas: int) -> float:
-    return total / parcelas if parcelas > 0 else total
+    return float(sum(custos.values()))
